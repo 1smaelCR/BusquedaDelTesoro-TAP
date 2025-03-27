0291a64aa49c72aedb997005c1e0834426864695
1smaelCR <cast.ro.ism.a@hotmail.com>
2025-03-27 04:48:16 -0600

    Juego casi terminado, falta el generador de acertijos.

--- src/ito/app/busquedadeltesoro/Main.java
+++ src/ito/app/busquedadeltesoro/Main.java
@@ -2,11 +2,9 @@ package ito.app.busquedadeltesoro;
 
 public class Main {
     public static void main(String[] args) {
-        // Crear la pantalla de carga
         PantallaCarga pantalla = new PantallaCarga();
         pantalla.setVisible(true);
 
-        // Crear el menú pero mantenerlo oculto
         MenuTesoro menu = new MenuTesoro();
         menu.setVisible(false); // No mostrar el menú inicialmente
 
@@ -14,9 +12,8 @@ public class Main {
         pantalla.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosed(java.awt.event.WindowEvent e) {
-                // Mostrar el menú y reproducir música de fondo
                 menu.setVisible(true);
-                menu.iniciarMusicaFondo(); // Inicia la música solo cuando el menú es visible
+                menu.iniciarMusicaFondo();
             }
         });
     }
